6f4c76b03b5c708d3590c4da78118397f24e5ff7
swaroop2603 <84812732+swaroop2603@users.noreply.github.com>
2023-10-01 23:08:16 +0530

    Add files via upload

---
+++ auction/auctionapp/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import User
+class userserializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields='__all__'
+
+
