6131be1df9ebe43abe898590a5d698e6bd81b04b
swaroop2603 <84812732+swaroop2603@users.noreply.github.com>
2023-11-09 12:12:15 +0530

    Delete nutzen directory

--- nutzen/nutzenapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username=models.CharField(max_length=255)
-    email=models.EmailField(max_length=255,unique=True)
-    password=models.CharField(max_length=255)
